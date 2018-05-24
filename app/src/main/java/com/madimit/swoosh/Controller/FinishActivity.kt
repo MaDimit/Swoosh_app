a6ae7e751a9916622453f007172672775b577479
Maxim D <mdimirtov@gmail.com>
2018-05-24 20:21:09 +0300

    Player parcelable added. Default icon changed.

--- app/src/main/java/com/madimit/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/madimit/swoosh/Controller/FinishActivity.kt
@@ -1,10 +1,10 @@
 package com.madimit.swoosh.Controller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.madimit.swoosh.Model.Player
 import com.madimit.swoosh.R
-import com.madimit.swoosh.Utilities.EXTRA_LEAGUE
-import com.madimit.swoosh.Utilities.EXTRA_SKILL
+import com.madimit.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
@@ -13,9 +13,8 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
 
-        searchLeaguesText.text = "Looking for $league $skill league near you..."
+        searchLeaguesText.text = "Looking for ${player.league} ${player.skill} league near you..."
     }
 }
